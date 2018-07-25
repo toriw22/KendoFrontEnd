e671ce7be3b9601d24c9f00ae89c66d80a90773c
Tori Rojas <toriw22@gmail.com>
2018-07-25 15:04:03 -0700

    added form for new device

--- src/app/pages/add-edit-device/add-edit-device.component.ts
+++ src/app/pages/add-edit-device/add-edit-device.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditDeviceComponent implements OnInit {
 
+  public genders: Array<{ text: string, value: number }> = [
+    { text: "Male", value: 1 },
+    { text: "Female", value: 2 },
+    { text: "Other", value: 3 }
+  ];
+
+  public gender: { text: string, value: number } = { text: "Female", value: 2 };
+  
   public title: string = 'Add a New Device';
 
   constructor() { }
